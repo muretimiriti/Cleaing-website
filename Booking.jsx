296fccffe6f1f3ea53db50ca956e32dd25c71c02
muretimiriti <95303226+muretimiriti@users.noreply.github.com>
2024-01-30 13:36:51 +0300

    Add files via upload
    Organised components that handle the various actions in the website ensuring a smooth transition and operation

---
+++ Booking.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const booking = () => {
+  return (
+    <div>booking</div>
+  )
+}
+
+export default booking
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Navbar from "./Navbar";
+import Booking from "./Booking";
+import Features from "./Features";
+import Services from "./Services";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Products from "./Products";
+import Hero from "./Hero";
+import FeedbackCard from "./Products";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export {
+  Navbar,
+  Booking,
+  Features,
+  Services,
+  CTA,
+  Footer,
+  Products,
+  Hero,
+  FeedbackCard
+};
