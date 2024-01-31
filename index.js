506f5a197c5fc677bd158c78145389b1fbe22da0
muretimiriti <95303226+muretimiriti@users.noreply.github.com>
2024-01-31 21:30:07 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,256 @@
-import Navbar from "./Navbar";
-import Booking from "./Booking";
-import Features from "./Features";
-import Services from "./Services";
-import CTA from "./CTA";
-import Footer from "./Footer";
-import Products from "./Products";
-import Hero from "./Hero";
-import FeedbackCard from "./Products";
+import {  facebook, instagram, linkedin, twitter, customized,carpetcleaning,ecofriendly,
+    carpetcleaningservice, fumigationimage, fumigationimages, housecleaning,
+    housecleanup, number1,sofacleaning,sofacleanup,mattresscleaning,steammattresscleaning  } from "../assets";
 
-export {
-  Navbar,
-  Booking,
-  Features,
-  Services,
-  CTA,
-  Footer,
-  Products,
-  Hero,
-  FeedbackCard
-};
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about us",
+    title: "About Us",
+  },
+  {
+    id: "services",
+    title: "Services",
+  },
+  {
+    id: "booking",
+    title: "Booking",
+  },
+  {
+    id: "contact us ",
+    title: "Contact Us",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: number1,
+    title: "Professional Cleaning Services",
+    content:
+      "We guarantee a meticulous and professional approach to every task.",
+  },
+  {
+    id: "feature-2",
+    icon: ecofriendly,
+    title: "Ecofriendly products",
+    content:
+      "Our commitment to sustainability is reflected in our use of eco-friendly cleaning products and practices.",
+  },
+  {
+    id: "feature-3",
+    icon: customized,
+    title: "Customized Cleaning Plans",
+    content:
+      " Our tailored cleaning plans allow you to customize our services based on your specific needs.",
+  },
+];
+
+
+
+export const Services = [
+  {
+    id: "Services-1",
+    image: carpetcleaning,
+    title: "Carpet cleaning",
+    content:
+      " Experience the joy of walking on soft, clean carpets, knowing that your indoor environment is healthier and more welcoming.",
+  },
+  {
+    id: "Services-2",
+    image: steammattresscleaning,
+    title: "Mattress cleaning",
+    content:
+      " Invest in your well-being with our mattress cleaning, and experience the comfort and peace of mind that come with a thoroughly cleaned sleeping surface.",
+  },
+  {
+    id: "Services-3",
+    image: sofacleaning,
+    title: "Sofa cleaning",
+    content:
+      "  Whether it's a beloved family couch or a stylish lounge suite, our service restores the beauty and comfort of your upholstery, leaving it fresh, vibrant, and free from contaminants",
+  },
+  {
+    id: "Services-4",
+    image: housecleaning,
+    title: "House cleaning",
+    content:
+      " Enjoy the benefits of a sparkling clean and organized home, where each room radiates freshness, creating a welcoming and comfortable atmosphere for you and your family.",
+  },
+  {
+    id: "Services-5",
+    image: fumigationimage,
+    title: "Fumigation",
+    content:
+      " Protect your property and create a pest-free environment for a healthier and more secure living or working space",
+  },
+];
+
+export const about = [
+  {
+    id: "about-1",
+    title: "About us ",
+    content:
+      " At VinnyFastSide Cleaning Services, we take pride in transforming living and working spaces into pristine havens of cleanliness. With a commitment to excellence and a passion for hygiene, we've become a trusted name in the cleaning industry. Our mission is to redefine cleaning standards, providing our clients with a level of service that exceeds expectations.",
+  },
+
+  {
+    id: "about-2",
+    title: "Our Values",
+    content:
+      " At the core of our business are values that drive every aspect of what we do. We believe in integrity, reliability, and a relentless pursuit of quality. Our team is dedicated to delivering not just clean spaces, but an experience that reflects our commitment to professionalism and customer satisfaction.",
+  },
+  
+  {
+    id: "about-3",
+    title: "Professional Expertise",
+    content:
+      "Our highly skilled and trained cleaning technicians bring years of experience to every job. We use the latest cleaning techniques, equipment, and eco-friendly products to ensure optimal results. From carpets and upholstery to mattresses and entire homes, our expertise covers a comprehensive range of cleaning services.",
+  },
+  {
+    id: "about-4",
+    title: "Customer-Centric Approach ",
+    content:
+      " We understand that each client is unique, with specific needs and preferences. Our customer-centric approach allows us to tailor our services to meet individual requirements. Whether it's a one-time deep clean, routine maintenance, or specialized cleaning, we work closely with our clients to deliver personalized solutions.",
+  },
+  {
+    id: "about-5",
+    title: "Your Trusted Cleaning Partner ",
+    content:
+      " Vinny FastSisde Cleaning Services is more than a cleaning company; we are your trusted partner in creating and maintaining clean, comfortable, and healthy spaces. Join us on the journey to a sparkling clean environment, where your satisfaction is our priority. Experience the Sparkling Homes difference today!",
+  },
+  
+
+
+];
+
+export const Products = [
+  {
+    id: "Products-1",
+    image: carpetcleaningservice,
+    title: "Carpet cleaning",
+    content:
+      " Our professional carpet cleaning service goes beyond surface dirt removal. Using advanced steam cleaning technology, we delve deep into the fibers, extracting allergens, dust mites, and stubborn stains. Our process not only rejuvenates your carpet's appearance but also enhances indoor air quality, making your home a healthier environment. With our eco-friendly cleaning solutions, we ensure a thorough clean without compromising the integrity of your carpets, leaving them soft, fresh, and vibrant.",
+  },
+  {
+    id: "Products-2",
+    image: mattresscleaning,
+    title: "Mattress cleaning",
+    content:
+      "Invest in a good night's sleep with our thorough mattress cleaning service. We understand the importance of a clean and hygienic sleep environment. Our mattress cleaning process involves the removal of dust mites, bacteria, and allergens, ensuring a fresh and sanitized sleeping surface. Using non-toxic solutions, we treat stains and odors, leaving your mattress not only clean but also free from harmful substances. Experience the difference a professionally cleaned mattress can make to your overall sleep quality.",
+  },
+  {
+    id: "Products-3",
+    image: sofacleanup,
+    title: "Sofa cleaning",
+    content:
+      "  Transform your living space with our specialized sofa cleaning service. Our skilled technicians employ a combination of deep cleaning methods and safe, effective detergents to revitalize your sofas. Whether dealing with fabric or leather, we tailor our approach to each material, removing dirt, odors, and allergens. Rest easy knowing that our meticulous process not only enhances the lifespan of your sofas but also creates a healthier, more inviting atmosphere for you and your family.",
+  },
+  {
+    id: "Products-4",
+    image: housecleanup,
+    title: "House cleaning",
+    content:
+      " Our comprehensive house cleaning service is designed to give you a spotless and organized home without the stress. Our skilled cleaning team tackles every corner, from top to bottom. We use environmentally friendly cleaning agents to sanitize surfaces, eliminate dust, and remove grime. From kitchens to bathrooms, bedrooms to living spaces, we leave no detail overlooked. Embrace a clean, clutter-free living space that promotes a healthier lifestyle and allows you to focus on the things that matter most.",
+  },
+  {
+    id: "Products-5",
+    image: fumigationimages,
+    title: "Fumigation",
+    content:
+      " Ensure a pest-free environment with our expert fumigation services. Our highly trained technicians conduct a thorough assessment of your property to identify potential infestation areas. Using safe and effective fumigants, we eradicate pests such as insects, rodents, and termites. Our process is designed to penetrate even the most hard-to-reach spaces, providing a long-lasting solution. Protect your home or business from the detrimental effects of pests and create a safe, hygienic environment for you, your family, or your employees.",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
